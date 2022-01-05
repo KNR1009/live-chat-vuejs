156749bbceb5dd2163c619e121d879b73701ce90
伊東和成 <itokazunari5375@icloud.com>
2022-01-05 23:10:42 +0900

    rootの設定

---
+++ src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ title }}</div>
+  <div>{{ subtitle }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "初めてのVue.jsアプリです！",
+      subtitle: "テスト",
+    };
+  },
+};
+</script>
