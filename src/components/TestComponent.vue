6876350eb79954b5a27d745278e8247361cebe22
伊東和成 <itokazunari5375@icloud.com>
2022-01-06 07:41:35 +0900

    add vue methods

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>これはコンポーネントから表示されています。</div>
+  <p>{{ messages }}</p>
+  <p>{{ isEnabled }}</p>
+  <button @click="onClick()">親のメソットを実行する</button>
+</template>
+
+<script>
+export default {
+  props: ["messages", "isEnabled"],
+  methods: {
+    onClick() {
+      this.$emit("toggle");
+    },
+  },
+};
+</script>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,28 @@
 <template>
   <div>{{ title }}</div>
-  <div>{{ subtitle }}</div>
+  <TestComponent
+    messages="親から渡ってきました"
+    :isEnabled="isEnabled"
+    @toggle="toggle"
+  />
 </template>
 
 <script>
+import TestComponent from "../components/TestComponent";
 export default {
+  components: {
+    TestComponent,
+  },
   data() {
     return {
       title: "初めてのVue.jsアプリです！",
       subtitle: "テスト",
     };
+  },
+  methods: {
+    toggle() {
+      alert("クリックしました");
+    },
   },
 };
 </script>
